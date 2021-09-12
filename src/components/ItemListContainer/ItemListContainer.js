dd6fe17d40f0800cd7b04823db010dbc44d14491
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-09-12 08:44:33 -0300

    av 12092021

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,14 +2,14 @@ import React from "react"
 import './itemlistcontainer.css'
 
 function ItemListContainer() {
-  let items = "elementos de la lista";
+  let gretting = "Hola acá van los elementos de la lista";
 
   return (
     <div className="itemList">
       <div className="wrapper">
         <div className="itemList-container">
           <div className="itemList-content">
-             <p>Acá van los {items}</p>
+             <p>Acá van los {gretting}</p>
           </div>
         </div>
       </div>
