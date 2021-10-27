4450164777f9c7648a2f0a4a012ce33b293b19f8
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-10-26 21:22:06 -0300

    progress

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -6,6 +6,8 @@ import { CartContext } from '../../CartContext'
 function CartWidget() {
   const { cantidad } = useContext(CartContext);
 
+  const class1 = 'svg-inline--fa fa-shopping-bag fa-w-14 shop-icon';
+  const class2 = 'svg-inline--fa fa-shopping-bag fa-w-14 shop-icon shake';
 
   return (
     <Link to={'/cart'}>
@@ -13,7 +15,7 @@ function CartWidget() {
         <span className="shop-number" style={{
           visibility: cantidad === 0 ? "hidden" : "visible"
         }}>{cantidad}</span>
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-bag" className="svg-inline--fa fa-shopping-bag fa-w-14 shop-icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="shopping-bag" className={ cantidad === 0 ? class1 : class2 } role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
           <path fill="currentColor" d="M352 160v-32C352 57.42 294.579 0 224 0 153.42 0 96 57.42 96 128v32H0v272c0 44.183 35.817 80 80 80h288c44.183 0 80-35.817 80-80V160h-96zm-192-32c0-35.29 28.71-64 64-64s64 28.71 64 64v32H160v-32zm160 120c-13.255 0-24-10.745-24-24s10.745-24 24-24 24 10.745 24 24-10.745 24-24 24zm-192 0c-13.255 0-24-10.745-24-24s10.745-24 24-24 24 10.745 24 24-10.745 24-24 24z"></path>
         </svg>
         <span className="shop-title">Go to cart</span>
--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
-
+import Message from '../Message/Message';
+import {Link} from 'react-router-dom';
 
 function OrderForm({saveOrder}) {
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+
+  const [nameMes, setNameMes] = useState('');
+  const [emailMes, setEmailMes] = useState('');
+  const [phoneMes, setPhoneMes] = useState('');
+
+  const [validated, setValidated] = useState(false);
  
   function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,:\s@"]+(\.[^<>()[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -14,15 +21,21 @@ function OrderForm({saveOrder}) {
 
   const validateAndSaveOrder = () => {
     if (name === '') {
-      console.log('Name is required.')
-    } 
+      setNameMes('Name is required.');
+    } else {
+      setNameMes('');
+    }
 
     if (!validateEmail(email)) {
-      console.log('Valid email is required.')
+      setEmailMes('Valid email is required.');
+    } else {
+      setEmailMes('');
     }
 
     if (phone === '') {
-      console.log('Phone is required.')
+      setPhoneMes('Phone is required.');
+    } else {
+      setPhone('');
     }
 
     if (name !== '' && validateEmail(email) && phone !== '') {
@@ -39,19 +52,24 @@ function OrderForm({saveOrder}) {
         <div className="form-group">
           <label htmlFor="name">Name:</label>
           <input type="text" name="name" id="name" onChange={ e => setName(e.target.value)} required maxLength="50"/>
+          <Message message={nameMes} />
         </div>
 
         <div className="form-group">
           <label htmlFor="email">Email:</label>
           <input type="email" name="email" id="email" onChange={ e => setEmail(e.target.value)} required maxLength="50"/>
+          <Message message={emailMes} />
         </div>
         
         <div className="form-group">
           <label htmlFor="phone">Contact number:</label>
-          <input type="text" name="phone" id="phone" onChange={ e => setPhone(e.target.value)}  required maxLength="12"/>
+          <input type="text" name="phone" id="phone" onChange={ e => setPhone(e.target.value)}  required maxLength="11"/>
+          <Message message={phoneMes} />
         </div>
         
-        <button className="btn-primary" onClick={validateAndSaveOrder} >Send order</button>
+        <Link to={"/cart/confirmation"} >
+          <button className="btn-primary" onClick={validateAndSaveOrder} >Send order</button>
+        </Link>
         
       </div>
   )
